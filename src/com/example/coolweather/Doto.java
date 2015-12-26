00163e159f6048056b0fcb02d3879d65682b6dcb
tangcan <doyoubest8@126.com>
2015-12-26 13:05:54 +0800

    提交实体类

---
+++ src/com/example/coolweather/Doto.java
@@ -0,0 +1,20 @@
+package com.example.coolweather;
+
+public class Doto {
+
+	public int getI() {
+		return i;
+	}
+	public void setI(int i) {
+		this.i = i;
+	}
+	public String getA() {
+		return a;
+	}
+	public void setA(String a) {
+		this.a = a;
+	}
+	private int i;
+	private String a;
+	
+}
